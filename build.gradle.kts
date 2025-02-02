52a879364ef092b7e764c0c3ac284764a110c918
Cannur Kartum <48458722+CannurKartum@users.noreply.github.com>
2025-02-02 22:33:14 +0100

    Dependencies added, model classes created.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,9 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("com.android.library") version "8.5.0" apply false
+    id("com.google.devtools.ksp") version "2.0.20-1.0.25" apply false
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
+    kotlin("plugin.serialization") version "2.0.20" apply false
 }
